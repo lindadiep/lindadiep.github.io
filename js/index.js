af6833d196a76bbd7778159809d938f0f109599f
lindadiep <lindadiep@Diana-Dieps-MacBook-Pro.local>
2016-02-15 19:22:37 -0500

    Worked on carousel
    It kind of works. Needs to loop around.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,53 @@
+
+
+// CAROUSEL!!!
+$(document).ready(function(){
+
+var images=[
+    "images/JS-2.jpg",
+    "images/JS-3.jpg",
+    "images/JS-5.jpg",
+    "images/JS-6.jpg"
+    ];
+
+var i=0;
+
+function changeImage(){
+	$("#carousel-Img").attr("src",images[i]);
+}
+
+function previousImage(){
+  i>0 ? changeImage(--i):0;
+}
+
+function nextImage(){
+  i+1<images.length ? changeImage(++i):1;
+}
+
+$(".carouselBtn[value='Next']").on("click",nextImage);
+$(".carouselBtn[value='Prev']").on("click",previousImage);
+
+});
+
+// Other js code to use:
+
 //Back to top scroll button:
 
 // http://jsfiddle.net/gilbitron/Lt2wH/
 // https://codyhouse.co/gem/back-to-top/
 // http://html-tuts.com/back-to-top-button-jquery/
+
+
+// JUMP TO ANCHOR!!!
+
+//var shiftWindow = function() {
+//    scrollBy(0, -50
+//    );
+
+// <body onload="load()">
+//window.addEventListener("hashchange", shiftWindow);
+
+//$('body').onload(function(){
+//    if (window.location.hash)
+//      shiftWindow();
+//    };

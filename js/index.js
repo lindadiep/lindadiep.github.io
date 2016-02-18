00eadc673b4e36c9e9e7598e14a854dd77b99a8c
Joni Halabi <joni@jhalabi.com>
2016-02-18 00:06:04 -0500

    Feedback for JS milestone.

--- js/index.js
+++ js/index.js
@@ -17,10 +17,36 @@ function changeImage(){
 }
 
 function previousImage(){
+    /*
+     This line below is shorthand for an if statement, but I do not think it's working
+     quite right.  I would advise writing out the full if statement.
+
+     Also, a weird quirk about using ++ or -- to add or subtract 1 from a variable.
+
+     If you type --i, it will subtract 1 from i first, and then pass that new number to your function.
+     If you type i--, it will pass i to your function and then subtract 1 from i.
+
+     So, if i == 0:
+
+        changeImage(--i) passes -1 to the function
+        changeImage(i--) passes 0 to the function
+
+        changeImage(++i) passes 1 to the function
+        changeImage(i++) passes 0 to the function
+
+     So, unless you have a specific reason for using --i or ++i, I would normally use i-- or i++
+
+     Weird, but important quirk.
+    */
+
   i>0 ? changeImage(--i):0;
 }
 
 function nextImage(){
+    /* 
+        See massive comment above; applies to this line too. 
+    */
+    
   i+1<images.length ? changeImage(++i):1;
 }
 

346e66dbb8ffb744e6dff66ad86a4f1e3a028428
Linda Diep <lxdiep07@gmail.com>
2016-05-07 15:09:35 -0400

    Refractored

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
 $(document).ready(function (){
 
-			$(".portfolio").click(showPortfolio);
+		$(".portfolio").click(showPortfolio);
 					function showPortfolio() {
 					$(".portfolio-links").slideToggle();
 				}
-			//
-			// $(".ga").click(showGa);
-			//  		function showGa() {
-			//  		$(".ga-links").slideToggle();
-			//  	}
-
-		  $(".contacts").click(showContacts);
-		  		function showContacts() {
-		  		$(".contacts-links").slideToggle();
+		
+		$(".contacts").click(showContacts);
+			  		function showContacts() {
+			  		$(".contacts-links").slideToggle();
 		 		}
 
 });

e2ec10f372e05c5e4da40d1a6706208ca494213d
Linda Diep <lxdiep07@gmail.com>
2016-05-10 19:24:14 -0400

    Refractured local js file

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 $(document).ready(function (){
 	
-	$(".portfolio").click(showPortfolio);
-		function showPortfolio() {
-		$(".portfolio-links").slideToggle();
 
-<<<<<<< HEAD
 		$(".portfolio").click(showPortfolio);
 					function showPortfolio() {
 					$(".portfolio-links").slideToggle();
@@ -14,11 +10,5 @@ $(document).ready(function (){
 			  		function showContacts() {
 			  		$(".contacts-links").slideToggle();
 		 		}
-=======
-	$(".contacts").click(showContacts);
-		function showContacts() {
-		$(".contacts-links").slideToggle();
-	}
->>>>>>> 15b1d2554a48481b1945e2fa39d553fe8e9375ea
 
 });

965d5e6bdf4481da0585496ccbe0d688808cd29f
Linda Diep <lxdiep07@gmail.com>
2016-05-10 19:32:35 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,14 @@
 $(document).ready(function (){
 	
 
-<<<<<<< HEAD
 		$(".portfolio").click(showPortfolio);
-					function showPortfolio() {
-					$(".portfolio-links").slideToggle();
+			function showPortfolio() {
+			$(".portfolio-links").slideToggle();
 				}
 		
 		$(".contacts").click(showContacts);
-			  		function showContacts() {
-			  		$(".contacts-links").slideToggle();
+			function showContacts() {
+			$(".contacts-links").slideToggle();
 		 		}
 
-=======
-	$(".contacts").click(showContacts);
-		function showContacts() {
-		$(".contacts-links").slideToggle();
-	}
->>>>>>> 2e958272e365ddb4a020aebfdaf59e6d3fc91aa7
 });

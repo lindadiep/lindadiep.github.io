2e958272e365ddb4a020aebfdaf59e6d3fc91aa7
Linda Diep <lxdiep07@gmail.com>
2016-05-10 19:19:05 -0400

    Refractured.

--- js/index.js
+++ js/index.js
@@ -4,21 +4,8 @@ $(document).ready(function (){
 		function showPortfolio() {
 		$(".portfolio-links").slideToggle();
 
-<<<<<<< HEAD
-		$(".portfolio").click(showPortfolio);
-					function showPortfolio() {
-					$(".portfolio-links").slideToggle();
-				}
-		
-		$(".contacts").click(showContacts);
-			  		function showContacts() {
-			  		$(".contacts-links").slideToggle();
-		 		}
-=======
 	$(".contacts").click(showContacts);
 		function showContacts() {
 		$(".contacts-links").slideToggle();
 	}
->>>>>>> 15b1d2554a48481b1945e2fa39d553fe8e9375ea
-
 });
